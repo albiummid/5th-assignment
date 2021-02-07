2c3c9faf35c48223da55df9607fdd26717f5f6c8
Albi Ummid <albi.ummid@gmail.com>
2021-02-07 17:42:19 +0600

    main + bonus theme completed.just have to optimize

--- javaScript.js
+++ javaScript.js
@@ -1,33 +1,82 @@
 document.getElementById("search").addEventListener("click",function(){
 loadData();
+const mealName = document.getElementById("meal-input").value = '';
 })
 const loadData = search =>{
     const mealName = document.getElementById("meal-input").value;
+    const searchResults = document.getElementById("search-results");
+    searchResults.innerHTML = '';
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`)
     .then(res => res.json())
     .then(data => displaySearch(data))
     .catch(error =>{
-        const searchDetails = document.getElementById("search-details");
+        const searchDetails = document.getElementById("meal-details");
         const notFoundMessage = `
-        <h4> Sorry! "${mealName}" is not available right now. </h4>
+        <h4 id= "error"> Sorry! "${mealName}" is not available right now. </h4>
         `;
         searchDetails.innerHTML= notFoundMessage;
+        const searchResults = document.getElementById("search-results");
+        searchResults.innerHTML = '';
+        const cleanMessage = setInterval(() => {
+            cleanUp();
+        }, 1500);
     })
+
     const displaySearch = meals =>{
         const mealsArray = meals.meals
         const searchResults = document.getElementById("search-results");
         mealsArray.forEach(meal => {
+            // console.log(meal);
             const mealDiv = document.createElement("div");
             mealDiv.className = "mealDiv";
             const mealInfo = `
-            <img src="${meal.strMealThumb} ">
+            <img onclick="displayDetails(${meal.idMeal})"src="${meal.strMealThumb} ">
             <br>
-            <a href="">${meal.strMeal} </a>
+            <a onclick="displayDetails(${meal.idMeal})" href=#search-details>${meal.strMeal} </a>
             `;
             mealDiv.innerHTML = mealInfo;
             searchResults.appendChild(mealDiv);
         });
-        }  
+        }         
 }
+
+const cleanUp = clean =>{
+    document.getElementById("error").innerText = "";
+} 
+
+
+const displayDetails = mealID =>{
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => renderDetails(data))
+}
+const renderDetails = meals =>{
+    const mealDetails = document.getElementById("meal-details");
+    const detailsArray = meals.meals[0];
+    console.log(meals.meals[0]);
+    mealDetails.innerHTML = `
+    <div id="details">
+    <img src="${detailsArray.strMealThumb}">
+    <h2>${detailsArray.strMeal}</h2>
+    <h4> Ingredients: <h4>
+    <ul>
+    <li>${detailsArray.strMeasure1} ${detailsArray.strIngredient1}</li> 
+    <li>${detailsArray.strMeasure2} ${detailsArray.strIngredient2}</li> 
+    <li>${detailsArray.strMeasure3} ${detailsArray.strIngredient3}</li> 
+    <li>${detailsArray.strMeasure4} ${detailsArray.strIngredient4}</li> 
+    <li>${detailsArray.strMeasure5} ${detailsArray.strIngredient5}</li> 
+    <li>${detailsArray.strMeasure6} ${detailsArray.strIngredient6}</li> 
+    <li>${detailsArray.strMeasure7} ${detailsArray.strIngredient7}</li> 
+    <li>${detailsArray.strMeasure8} ${detailsArray.strIngredient8}</li> 
+    <li>${detailsArray.strMeasure9} ${detailsArray.strIngredient9}</li> 
+    <li>${detailsArray.strMeasure10} ${detailsArray.strIngredient10}</li> 
+    <li>${detailsArray.strMeasure11} ${detailsArray.strIngredient11}</li> 
+      
+     </ul>
+    </div>
+    `
+} 
+
 
 
